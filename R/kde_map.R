961b590c8864934d2e14a44233e2b386163256b2
Jamie Spaulding <jspauldi@mix.wvu.edu>
2019-12-01 10:10:03 +0000

    version 0.1.0

---
+++ R/kde_map.R
@@ -0,0 +1,58 @@
+## kde_map
+## Jamie Spaulding
+
+#' Kernel Density Estimation and Heat Map Generation for Crime Incidents
+#' @description This function computes a kernel density estimate of crime
+#'     incident locations and returns a 'Leaflet' map of the incidents. The data
+#'     is based on the Chicago Police Department RMS structure and populates
+#'     pop-up windows with the incident location for each incident.
+#' @param data Data frame of crime or RMS data. See provided Chicago Data Portal
+#'     example for reference
+#' @return A \pkg{Leaflet} map with three layers: an 'ESRI' base-map, all crime
+#'     incidents plotted (with incident info pop-up windows), and a kernel
+#'     density estimate of those points.
+#' @author Jamie Spaulding, Keith Morris
+#' @keywords spatial methods hplot dynamic
+#' @examples
+#' #Using provided dataset from Chicago Data Portal:
+#' data(crimes)
+#' crimes <- head(crimes, 1000)
+#' library('leaflet') # needed to install basemap providers
+#' kde_map(crimes)
+#' @importFrom grDevices contourLines
+#' @importFrom grDevices heat.colors
+#' @importFrom KernSmooth bkde2D
+#' @importFrom sp Polygons
+#' @importFrom sp Polygon
+#' @importFrom sp SpatialPolygons
+#' @importFrom stats bw.nrd0
+#' @import leaflet
+#' @import htmltools
+#' @export
+kde_map <- function(data){
+  lat <- as.numeric(data$latitude)
+  lon <- as.numeric(data$longitude)
+  bwlat <- stats::bw.nrd0(lat) #calculate bandwidth (lat) for KDE function
+  bwlon <- stats::bw.nrd0(lon) #calculate bandwidth (lon) for KDE function
+  kde <- KernSmooth::bkde2D(cbind(lon, lat), # calculates the KDE using calculated bandwidths
+                            bandwidth=c(bwlon, bwlat), gridsize = c(100, 100))
+  CL <- grDevices::contourLines(kde$x1 , kde$x2 , kde$fhat) #uses KDE to create contour lines
+
+  # Extract Contour Line Levels -----
+  LEVS <- as.factor(sapply(CL, `[[`, "level"))
+  NLEV <- length(levels(LEVS))
+
+  # Convert Contour Lines To Polygons -----
+  pgons <- lapply(1:length(CL), function(i)
+    sp::Polygons(list(sp::Polygon(cbind(CL[[i]]$x, CL[[i]]$y))), ID = i))
+  spgons = sp::SpatialPolygons(pgons)
+  map <- leaflet::leaflet(data) %>% leaflet::addProviderTiles(leaflet::providers$Esri.NatGeoWorldMap) %>%
+    leaflet::addScaleBar(position = "bottomright") %>%
+    leaflet::addPolygons(data = spgons, color = grDevices::heat.colors(NLEV, NULL)[LEVS]) %>%
+    leaflet::addCircles(lon, lat, popup = paste("Case Number:", data$case_number, "<br/>"
+                                       ,"Description:", data$description, "<br/>"
+                                       ,"District:", data$district, "<br/>"
+                                       ,"Beat:", data$beat, "<br/>"
+                                       ,"Date:", data$date), color ="purple")
+  return(map)
+}
---
+++ R/ts_daily_decomp.R
@@ -0,0 +1,57 @@
+## ts_daily_decomp
+## Jamie Spaulding
+
+#' Time Series Forecast and Decomposition for Daily Crime Data
+#' @description This function transforms daily crime count data and plots the
+#'     resultant components of a time series which has been decomposed into
+#'     seasonal, trend, and irregular components using Loess smoothing. Holt
+#'     Winters exponential smoothing is also performed for inproved trend
+#'     resolution since data is in a daily format.
+#' @param data Data frame of crime or RMS data. See provided Chicago Data Portal
+#'     example for reference
+#' @param start Start date for the time series being analyzed. The format is as
+#'     follows: c('year', 'month', 'day'). See example below for reference.
+#' @return Returns an object of class "stl" with the following components:
+#'
+#' time.series: a multiple time series with columns seasonal, trend and remainder.
+#'
+#' weights: the final robust weights (all one if fitting is not done robustly).
+#'
+#' call: the matched call.
+#'
+#' win: integer (length 3 vector) with the spans used for the "s", "t", and "l" smoothers.
+#'
+#' deg: integer (length 3) vector with the polynomial degrees for these smoothers.
+#'
+#' jump: integer (length 3) vector with the ‘jumps’ (skips) used for these smoothers.
+#'
+#' inner: number of inner iterations
+#' @author Jamie Spaulding, Keith Morris
+#' @keywords ts
+#' @examples
+#' #Using provided dataset from Chicago Data Portal:
+#' data(crimes)
+#' test <- ts_daily_decomp(data = crimes, start = c(2017, 1, 1))
+#' plot(test)
+#' @importFrom lubridate parse_date_time
+#' @importFrom stats HoltWinters
+#' @importFrom stats stl
+#' @importFrom stats ts
+#' @export
+ts_daily_decomp <- function(data, start){
+  data$date <- as.Date(data$date, "%m/%d/%Y %H:%M")
+  dates <- unique(data$date) #get unique dates
+  z <- as.data.frame(table(data$date))
+  z$Var1 <- lubridate::parse_date_time(z$Var1, orders = "%Y-%m-%d") #parses an z into POSIXct date-time object
+  z$Var1 <- z[order(as.Date(z$Var1)),] #order by date
+  dat <- z[,1]
+  dataseries <- stats::ts(dat$Freq, frequency=365, start=c(start,1)) #frequency=12 months, specify start date and that data is monthly (2001,1)
+
+  ## Decomposition -----
+  ds_decomposed <- stats::stl(dataseries, "per") #plot decomposed times series
+
+  ## Holt winters Exponsntial Smoothing of Seasonality -----
+  tssmooth <- stats::HoltWinters(ds_decomposed$time.series[,1], beta=FALSE, gamma=FALSE)
+  ds_decomposed$time.series[2:nrow(ds_decomposed$time.series),1] <- tssmooth$fitted[,1]
+  return(ds_decomposed)
+}
